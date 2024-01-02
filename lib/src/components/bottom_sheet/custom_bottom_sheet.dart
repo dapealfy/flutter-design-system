637201ba9fbdf129ef3abd34523f665a9f75364a
wahyuabidarifiyanto <wahyu.arifiyanto@koinworks.com>
2024-01-02 16:19:32 +0700

    CON-1176: create funDsBottomSheet and funDsCustomBottomSheet

---
+++ lib/src/components/bottom_sheet/custom_bottom_sheet.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_design_system/funds.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FunDsCustomBottomSheet extends StatelessWidget {
+  final Widget child;
+
+  const FunDsCustomBottomSheet({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  static void showBottomSheet({
+    Key? key,
+    required BuildContext context,
+    required Widget child,
+  }) {
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: FunDsColors.colorWhite,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(12.r),
+          topRight: Radius.circular(12.r),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return FunDsCustomBottomSheet(
+          key: key,
+          child: child,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      key: key,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 8.h),
+              child: Center(
+                child: Container(
+                  width: 30.w,
+                  height: 4.h,
+                  decoration: ShapeDecoration(
+                    color: FunDsColors.colorNeutral200,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1160.r),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 12.h, left: 12.w),
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: Icon(
+                  Icons.close,
+                  size: 20.h,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 12.h,
+                horizontal: 20.w,
+              ),
+              child: child,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/src/components/components.dart
+++ lib/src/components/components.dart
@@ -1,5 +1,7 @@
 export 'atoms/atoms.dart';
 export 'button/button.dart';
 export 'button/group_button.dart';
+export 'bottom_sheet/bottom_sheet.dart';
+export 'bottom_sheet/custom_bottom_sheet.dart';
 export 'input_field/funds_text_controller.dart';
 export 'input_field/text_field.dart';
