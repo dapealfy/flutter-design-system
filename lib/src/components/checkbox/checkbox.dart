c8c1593be21c215d528f02b440dbe33531f1ae0f
wahyuabidarifiyanto <wahyu.arifiyanto@koinworks.com>
2024-01-19 14:23:16 +0700

    CON-1198: add left padding checkbox

--- lib/src/components/checkbox/checkbox.dart
+++ lib/src/components/checkbox/checkbox.dart
@@ -55,18 +55,21 @@ class _FunDsCheckboxState extends State<FunDsCheckbox> {
               },
         title: widget.title == null
             ? null
-            : Text(
-                widget.title ?? '',
-                style: FunDsTypography.body14.copyWith(
-                  color: widget.variant == FunDsCheckboxVariant.disabled
-                      ? FunDsColors.colorNeutral500
-                      : FunDsColors.colorNeutral900,
+            : Padding(
+              padding: EdgeInsets.only(left: 8.h),
+              child: Text(
+                  widget.title ?? '',
+                  style: FunDsTypography.body14.copyWith(
+                    color: widget.variant == FunDsCheckboxVariant.disabled
+                        ? FunDsColors.colorNeutral500
+                        : FunDsColors.colorNeutral900,
+                  ),
                 ),
-              ),
+            ),
         subtitle: widget.subtitle == null
             ? null
             : Padding(
-                padding: EdgeInsets.only(top: 8.h),
+                padding: EdgeInsets.only(top: 8.h, left: 8.h),
                 child: Text(
                   widget.subtitle ?? '',
                   style: FunDsTypography.body14.copyWith(
