41243a2102f4a349421fd818b50dab96fde79e3c
Deni Al Farizi <deni.farizi@amartha.com>
2024-02-26 17:47:08 +0700

    CON-1302: create list tile component

--- lib/src/components/button/radio_button.dart
+++ lib/src/components/button/radio_button.dart
@@ -14,7 +14,7 @@ class FunDsRadioButton<T> extends StatelessWidget {
     Key? key,
     required this.value,
     required this.selectedValue,
-    required this.label,
+    this.label,
     this.onChanged,
     this.disabled,
     this.helper,
@@ -48,44 +48,58 @@ class FunDsRadioButton<T> extends StatelessWidget {
       return null;
     }
 
-    return ListTileTheme.merge(
-      horizontalTitleGap: 8.w,
-      minLeadingWidth: 8.w,
-      titleAlignment: ListTileTitleAlignment.top,
-      child: RadioListTile<T>(
-        value: value,
-        groupValue: selectedValue,
-        activeColor: FunDsColors.colorPrimary,
-        contentPadding: const EdgeInsets.all(0),
-        overlayColor: MaterialStateProperty.resolveWith(getOverlayColor),
-        fillColor: MaterialStateProperty.resolveWith(getColor),
-        hoverColor: FunDsColors.colorPrimary,
-        visualDensity: const VisualDensity(
-          horizontal: VisualDensity.minimumDensity,
-          vertical: VisualDensity.minimumDensity,
-        ),
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        splashRadius: 12.r,
-        onChanged: disabled ?? false
-            ? null
-            : ((value) {
-                onChanged?.call(value);
-              }),
-        title: Text(
-          label ?? '',
-          key: const Key('label'),
-          style: FunDsTypography.body14,
-        ),
-        subtitle: helper == null
-            ? null
-            : Text(
-                helper ?? '',
-                key: const Key('helper'),
-                style: FunDsTypography.body14.copyWith(
-                  color: FunDsColors.colorTextCaption,
-                ),
+    return label == null && helper == null
+        ? Radio(
+            value: value,
+            groupValue: selectedValue,
+            onChanged: onChanged,
+            activeColor: FunDsColors.colorPrimary,
+            hoverColor: FunDsColors.colorPrimary,
+            splashRadius: 12.r,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            visualDensity: const VisualDensity(
+              horizontal: VisualDensity.minimumDensity,
+              vertical: VisualDensity.minimumDensity,
+            ),
+          )
+        : ListTileTheme.merge(
+            horizontalTitleGap: 8.w,
+            minLeadingWidth: 8.w,
+            titleAlignment: ListTileTitleAlignment.top,
+            child: RadioListTile<T>(
+              value: value,
+              groupValue: selectedValue,
+              activeColor: FunDsColors.colorPrimary,
+              contentPadding: const EdgeInsets.all(0),
+              overlayColor: MaterialStateProperty.resolveWith(getOverlayColor),
+              fillColor: MaterialStateProperty.resolveWith(getColor),
+              hoverColor: FunDsColors.colorPrimary,
+              visualDensity: const VisualDensity(
+                horizontal: VisualDensity.minimumDensity,
+                vertical: VisualDensity.minimumDensity,
               ),
-      ),
-    );
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              splashRadius: 12.r,
+              onChanged: disabled ?? false
+                  ? null
+                  : ((value) {
+                      onChanged?.call(value);
+                    }),
+              title: Text(
+                label ?? '',
+                key: const Key('label'),
+                style: FunDsTypography.body14,
+              ),
+              subtitle: helper == null
+                  ? null
+                  : Text(
+                      helper ?? '',
+                      key: const Key('helper'),
+                      style: FunDsTypography.body14.copyWith(
+                        color: FunDsColors.colorTextCaption,
+                      ),
+                    ),
+            ),
+          );
   }
 }
